896fe1453146505ab8189a36905b9b51ac05bbd1
Thorek <75499293+Thorek777@users.noreply.github.com>
2021-07-25 21:51:35 +0200

    Moved Client to C++20 and more

---
+++ Client/Client/Source/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "Utils/Boost/Boost_Connector.hpp"
+
+int main()
+{
+	system("pause");
+	return 0;
+}
---
+++ Client/Client/Source/Utils/Boost/Boost_Connector.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace Boost
+{
+	int ExecuteConnect();
+}
